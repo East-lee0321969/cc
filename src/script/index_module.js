6da825bf331048d0733045d5272c37b3a9f9bece
East_lee0321969 <east-lee0321969@163.com>
2020-12-15 23:58:02 +0800

    今天写了列表页结构和渲染和首页完善

--- src/script/index_module.js
+++ src/script/index_module.js
@@ -2,19 +2,126 @@ define([], () => {
     return {
         init: function() {
             //获取元素
-            // const $banner = $('.banner');
-            // const $ulist = $('.banner ul'); 
-            // const $piclist = $('.banner ul li');
-            // const $btnlist = $('.banner ol li');
-             const $white=$('.white_right ul')
-            // let timer=null;
-            // let $num=0;
+            const $banner = $('.banner');
+            const $ulist = $('.wrap_list');
+            const $piclist = $('.lunbo');
+            const $btnlist = $('.wrap_ol li');
+            const $white = $('.white_right ul');
+            const $item_list = $('.item_list li')
+            const $menu = $('.menu .item');
+            let $num = 0;
+            let timer = null;
 
+            //轮播图
             //鼠标移入按钮改变颜色，图片改变
-            // $btnlist.on('click',function(){
-            //     $num=$(this).index();
-            //     $btnlist.addClass('active').siblings('li').removeClass('active');
-            // })
+            $btnlist.on('mouseover', function() {
+                $num = $(this).index();
+                $(this).addClass('active').siblings('li').removeClass('active');
+                $piclist.eq($(this).index()).addClass('show').siblings('.lunbo').removeClass('show');
+            })
+
+            //鼠标移入停止运动
+            $ulist.hover(function() {
+                clearInterval(timer);
+            }, function() {
+                timer = setInterval(function() {
+                    $num++;
+                    //判断索引
+                    if ($num === $btnlist.size()) {
+                        $num = 0;
+                    }
+                    $btnlist.eq($num).addClass('active').siblings('li').removeClass('active');
+                    $piclist.eq($num).addClass('show').siblings('.lunbo').removeClass('show');
+                }, 3000);
+            })
+
+            //打开定时器
+            timer = setInterval(function() {
+                $num++;
+                //判断索引
+                if ($num === $btnlist.size()) {
+                    $num = 0;
+                }
+                $btnlist.eq($num).addClass('active').siblings('li').removeClass('active');
+                $piclist.eq($num).addClass('show').siblings('.lunbo').removeClass('show');
+            }, 3000);
+
+
+            //二级菜单
+
+            $item_list.hover(function() {
+                $menu.show();
+                $(this).addClass('active').siblings('li').removeClass('active');
+                $menu.eq($(this).index()).show().siblings('.item').hide();
+            }, function() {
+                $menu.hide();
+            })
+            $menu.hover(function() {
+                $(this).show();
+            }, function() {
+                $(this).hide();
+            });
+
+
+            //楼梯效果
+            var $stairway = $('.stairway');
+            var $stair = $('.stairway li').not('.last');
+            var $whitej = $('.whitej');
+
+            function scroll() {
+                var $scrolltop = $(window).scrollTop(); //获取滚动条的top值
+                if ($scrolltop >= 1314) {
+                    $stairway.show();
+                } else {
+                    $stairway.hide();
+                }
+                $whitej.each(function(index, element) {
+                    var $whitejtop = $(element).offset().top;
+                    if ($whitejtop >= $scrolltop) {
+                        $stair.removeClass('active');
+                        $stair.eq(index).addClass('active');
+                    }
+                })
+            }
+            scroll();
+
+
+            $(window).on('scroll', function() {
+                scroll();
+            });
+
+
+            $stair.on('click', function() {
+                $(window).off('scroll');
+                //求出每个楼层的top值。
+                $(this).addClass('active').siblings('li').removeClass('active');
+                var $whitejtop = $whitej.eq($(this).index()).offset().top;
+                $('html').animate({
+                    scrollTop: $whitejtop
+                }, function() {
+                    $(window).on('scroll', function() {
+                        scroll();
+                    });
+                });
+            });
+
+
+            //回到顶部
+            $('.last').on('click', function() {
+                $('html').animate({
+                    scrollTop: 0
+                });
+            });
+
+
+
+
+
+
+
+
+
+
             $.ajax({
                 url: 'http://localhost:8080/dashboard/projectname/php/index1.php',
                 dataType: 'json'
@@ -30,8 +137,8 @@ define([], () => {
                 </li>
             `;
                 });
-               $white.html($strhtml);
+                $white.html($strhtml);
             });
+        }
     }
-}
 });
--- src/script/main.js
+++ src/script/main.js
@@ -1,5 +1,4 @@
 require.config({
-    baseUrl: '', //配置js文件路径的公共部分，包括本地的文件。
     paths: { //path:给路径内部的代码设置一个名称，名称就是最后使用的模块的名称。
         'jquery': 'https://cdn.bootcdn.net/ajax/libs/jquery/1.12.4/jquery.min', //扩展名一定要省略
         'jlazyload': 'https://cdn.bootcdn.net/ajax/libs/jquery.lazyload/1.9.1/jquery.lazyload.min',
@@ -8,18 +7,17 @@ require.config({
     shim: { //让不支持AMD的模块支持AMD
         'jlazyload': {
             deps: ['jquery'], //deps:表示当前的模块依赖那个模块。
-            exports: ['jquery_lazyload'] //给模块另外取一个别名。
+            exports: 'jquery_lazyload' //给模块另外取一个别名。
         },
         'jcookie': {
             deps: ['jquery'], //deps:表示当前的模块依赖那个模块。
-            exports: ['jquery_cookie'] //给模块另外取一个别名。
+            exports: 'jquery_cookie' //给模块另外取一个别名。
         }
     }
 });
 
 
 require(['jquery'], function() {
-    console.log($);
     // //获取script标签里面的自定义属性targetpage的值
     // console.log($('#currentpage').attr('data-orign'));
     let $modulepage = $('#currentpage').attr('data-origin');
